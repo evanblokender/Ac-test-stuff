ea90da44b8fcfdcfcbaadd069049efef24294fdf
evanblokender <157309371+evanblokender@users.noreply.github.com>
2026-01-04 21:45:55 -0500

    Create UIViewController+Hook.m

---
+++ Sources/UIViewController+Hook.m
@@ -0,0 +1,29 @@
+#import <UIKit/UIKit.h>
+#import "CirclePopup.h"
+#import <objc/runtime.h>
+
+@implementation UIViewController (Hook)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class class = [UIViewController class];
+
+        SEL originalSelector = @selector(viewDidAppear:);
+        SEL swizzledSelector = @selector(swizzled_viewDidAppear:);
+
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+
+        method_exchangeImplementations(originalMethod, swizzledMethod);
+    });
+}
+
+- (void)swizzled_viewDidAppear:(BOOL)animated {
+    [self swizzled_viewDidAppear:animated]; // calls original viewDidAppear
+
+    // Install overlay on first view controller
+    [CirclePopup installOverlayIfNeeded:self];
+}
+
+@end
